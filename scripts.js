9b432b8315e2b550f90877c42cf3356e166c88be
sielock <aaboarder@gmail.com>
2021-05-13 15:52:37 -0600

    bugfix causing page to scroll when menu clicked

--- scripts.js
+++ scripts.js
@@ -1,49 +1,49 @@
-function toggleMenu() {
-    let navbar = document.querySelector('#nav-bar')
-    navbar.classList.toggle('show')
+const navbar = document.querySelector('#nav-bar');
+const toggleMenu = () => {
+    navbar.classList.toggle('show');
+}
+
+let scrollPosition = 0;
+const isScrollingDown = () => {
+  let currentScrolledPosition = window.scrollY || window.pageYOffset;
+
+  if(currentScrolledPosition > scrollPosition) {
+    scrollPosition = currentScrolledPosition;
+    return true;
+  } 
+  else {
+    scrollPosition = currentScrolledPosition;
+    return false;
   }
+}
 
-  let scrollPosition = 0;
-  const isScrollingDown = () => {
-    let currentScrolledPosition = window.scrollY || window.pageYOffset;
-
-    if(currentScrolledPosition > scrollPosition) {
-      scrollPosition = currentScrolledPosition;
-      return true;
-    } 
-    else {
-      scrollPosition = currentScrolledPosition;
-      return false;
-    }
+const nav = document.querySelector('nav');
+const handleNavScroll = () => {
+  if(isScrollingDown()) {
+    nav.classList.add('scroll-down');
+    nav.classList.remove('scroll-up');
   }
-
-  const nav = document.querySelector('nav');
-  const handleNavScroll = () => {
-    if(isScrollingDown()) {
-      nav.classList.add('scroll-down');
-      nav.classList.remove('scroll-up');
-    }
-    else {
-      nav.classList.add('scroll-up');
-      nav.classList.remove('scroll-down');
-    }
+  else {
+    nav.classList.add('scroll-up');
+    nav.classList.remove('scroll-down');
   }
+}
 
-  var throttleTimer;
-  const throttle = (callback, time) => {
-    if(throttleTimer) { return; }
+var throttleTimer;
+const throttle = (callback, time) => {
+  if(throttleTimer) { return; }
 
-    throttleTimer = true;
-    setTimeout(() => {
-      callback();
-      throttleTimer = false;
-    }, time);
+  throttleTimer = true;
+  setTimeout(() => {
+    callback();
+    throttleTimer = false;
+  }, time);
+}
+
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+window.addEventListener("scroll", () => {
+  if(mediaQuery && !mediaQuery.matches) {
+    throttle(handleNavScroll, 200)
   }
-
-  const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
-
-  window.addEventListener("scroll", () => {
-    if(mediaQuery && !mediaQuery.matches) {
-      throttle(handleNavScroll, 200)
-    }
-  });
+});
